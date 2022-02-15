158b0c54feb6063a57483e06a40a8aa6b0926ef5
Archon <archon.sh@gmail.com>
2022-02-15 17:40:42 +0100

    UseFetch hook added.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
+import useFetch from 'use-http'
+
 import './About.css';
 
 function About() {
+  const { loading, error, data = [] } = useFetch('https://jsonplaceholder.typicode.com/todos/1', null, []);
+
   return (
-    <div className="About">
-      Just about page! Nothing fancy! Come back to <Link to="/">main</Link>.
-    </div>
+    <>
+      {loading ? (
+        <>loading...</>
+      ) : (
+        <div className="About">
+          Just about page! Nothing fancy! Come back to <Link to="/">main</Link>.
+        </div>
+      )}
+    </>
   );
 }
 
